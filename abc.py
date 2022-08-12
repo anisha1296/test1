07e7663eb0e15e09b8cf5f5083b949821a9d9fc1
anisha <anishaborah96@gmail.com>
2022-08-12 10:04:26 +0530

    intial commit

---
+++ abc.py
@@ -0,0 +1,11 @@
+
+x=2
+y=3
+z=y%x
+
+	if z==0:
+		print("y divisible by x")
+	else:
+		print("y not divisble by x")
+
+print ("Hi guys")
---
+++ new.py
@@ -0,0 +1,4 @@
+
+print("Hi there")
+print("Welcome to the new world")
+print ("This is a python code")
